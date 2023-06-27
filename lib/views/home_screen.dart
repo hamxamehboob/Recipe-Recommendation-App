8df137fbfdfa8f82075f6f8498e9f5500953e054
hamxamehboob <hamzamehboob2003@gmail.com>
2023-06-27 15:36:25 +0500

    Home Screen UI completed

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_recommendation_app/constants/images.dart';
+import 'package:recipe_recommendation_app/views/information_screen.dart';
+import 'package:recipe_recommendation_app/widgets/recipe_cart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,33 +17,75 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.all(size.height * 0.01),
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFF4F5F9),
-                  border: Border.all(color: const Color(0xFFF4F5F9)),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: TextField(
-                  style: const TextStyle(color: Colors.black, fontSize: 15),
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(vertical: 17),
-                    prefixIcon: Icon(Icons.search, size: size.height * 0.04),
-                    hintText: 'Search keywords..',
-                    hintStyle: const TextStyle(
-                      color: Color(0xFF8F8F9E),
-                      fontSize: 15,
-                      fontFamily: 'Poppins',
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(size.height * 0.01),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF4F5F9),
+                    border: Border.all(color: const Color(0xFFF4F5F9)),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: TextField(
+                    style: const TextStyle(color: Colors.black, fontSize: 15),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 17),
+                      prefixIcon: Icon(Icons.search, size: size.height * 0.04),
+                      hintText: 'Search keywords..',
+                      hintStyle: const TextStyle(
+                        color: Color(0xFF8F8F9E),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                      ),
+                      border: InputBorder.none,
                     ),
-                    border: InputBorder.none,
                   ),
                 ),
-              ),
-              Image.asset(homeScreenImage),
-            ],
+                Image.asset(homeScreenImage),
+                SizedBox(
+                  height: size.height * .02,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      'Featured products',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.arrow_forward,
+                        color: Color(0xFF868889),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: size.height * .02,
+                ),
+                GridView.count(
+                  physics: const ScrollPhysics(),
+                  childAspectRatio: size.height / 800,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: size.width / size.height,
+                  shrinkWrap: true,
+                  children: const [
+                    RecipeCart(),
+                    RecipeCart(),
+                    RecipeCart(),
+                    RecipeCart(),
+                    RecipeCart(),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/recipe_cart.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../constants/images.dart';
+
+class RecipeCart extends StatefulWidget {
+  const RecipeCart({super.key});
+
+  @override
+  State<RecipeCart> createState() => _RecipeCartState();
+}
+
+class _RecipeCartState extends State<RecipeCart> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    return Column(
+      children: [
+        Container(
+          width: size.width * .4,
+          color: const Color.fromARGB(255, 237, 236, 236),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {});
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 120, top: 10),
+                  child: Icon(Icons.favorite_border),
+                ),
+              ),
+              CircleAvatar(
+                maxRadius: size.height * .08,
+                backgroundImage: AssetImage(homeScreenImage),
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(
+                height: size.height * .01,
+              ),
+              const Text(
+                'Pasta',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 78, 206, 78),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(
+                height: size.height * .01,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
